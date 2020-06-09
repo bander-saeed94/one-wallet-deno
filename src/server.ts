8c69bcc7b00b216b6fc656fbff1bf4d073e29f95
Bander Alshammari <bannadr1@hotmail.com>
2020-06-09 17:03:04 +0300

    remove direct pg connect, update readme

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,6 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import UserRouter from "./api/routes/user-route.ts";
 import LoginRouter from "./api/routes/login-route.ts";
 import "./db/load-db-models.ts";
-import { connectPg, closePg } from "./bootstrap/pg-client-bootstrap.ts";
-
-try {
-  await connectPg();
-  console.log("connect to db success");
-} catch (error) {
-  console.log(error);
-  throw new Error("exist app");
-}
 
 const app = new Application();
 
